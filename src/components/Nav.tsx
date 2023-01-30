806e391be137cabd3d896a0253c9bf2254d382bd
Jason Ren <jasoncren22@gmail.com>
2023-01-29 22:58:40 -0500

    navbar comments

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,9 @@ import { connect } from "react-redux"
 import Sidebar from "./Sidebar"
 import Logo from '../icons/Logo'
 
+/**
+ * Nav contains the logo and other navbar components.
+ */
 const Nav = () => (
   <div className="h-full bg-white pt-2 shadow-md py-auto align-middle flex flex-row">
     <div className="ml-4 text-3xl flex w-full font-medium tracking-wide">
